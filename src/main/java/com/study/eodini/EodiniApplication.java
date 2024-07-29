dc22673ba2a34c077ed8be53a8e1bb81a8baed54
김혁준 <kimhyeokjun@gimhyeogjun-ui-MacBookAir.local>
2024-07-29 14:14:06 +0900

    gitIgnore setting add

---
+++ src/main/java/com/study/eodini/EodiniApplication.java
@@ -0,0 +1,13 @@
+package com.study.eodini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EodiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EodiniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/eodini/EodiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.eodini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EodiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
